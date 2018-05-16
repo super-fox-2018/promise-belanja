855cddba2a94de23fb4c2c7fd4c229f05ff95ca1
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-16 18:36:52 +0700

    update with promise

--- beli.js
+++ beli.js
@@ -1,15 +1,23 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
-    }
-  }, obj.waktu);
+function beli(uang, food){
+  return new Promise(function(resolve, reject) {
+    
+    console.log(`Saya pergi membeli ${food}`)
+    setTimeout(function(){
+
+      var kembalian = uang
+      for(let i = 0; i < 5; i++) {
+        kembalian -= food[i].harga
+      }
+      
+      if (kembalian > 0) {
+        console.log(`Saya sudah membeli ${food} uang kembaliannya ${kembalian}`);
+        resolve(kembalian);
+      } else {
+        reject(`uang ga cukup nih buat beli ${food}`);
+      }
+    }, 1000);
+  })
+  
 }
 
 module.exports = beli;
