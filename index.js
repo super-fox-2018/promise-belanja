48404a59a45c23808773d49f03654debf22c5431
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-05-16 11:34:16 +0700

    Promise Belanja

--- index.js
+++ index.js
@@ -7,25 +7,27 @@ var obj3 = {item: 'Pasta Gigi', harga: 13000, waktu: 300};
 var obj4 = {item: 'Conditioner', harga: 10000, waktu: 300};
 var obj5 = {item: 'Kapas', harga: 9000, waktu: 50};
 
-  beli(uang, obj1, function(kembalian) {
-    uang = kembalian;
-    beli(uang, obj2, function(kembalian) {
-      uang = kembalian;
-      beli(uang, obj3, function(kembalian) {
-        uang = kembalian;
-        beli(uang, obj4, function(kembalian) {
-          uang = kembalian;
-          beli(uang, obj5, function(kembalian) {
-            uang = kembalian;
-          });
-        });
-      });
-    });
-  });
+beli(uang, obj1)
+.then(function(kembalian){
+    return beli(kembalian, obj2)
+})
+.then(function(kembalian){
+    return beli(kembalian, obj3)
+})
+.then(function(kembalian){
+    return beli(kembalian, obj4)
+})
+.then(function(kembalian){
+    return beli(kembalian, obj5)
+})
+.catch(function(message){
+    console.log(message)
+})
+
 
 /*
 NOTE
-di file beli.js line 9 yg awalnya kembalian cuma &{kembalian}, saya gnti ke ${uang}.
+di file beli.js line 9 yg awalnya kembalian cuma &{kembalian}, saya gnti ke sisa uang cuma ${uang}.
 karena kalau kembalian nanti jadinya -xxx sesuai dgn kembalian dikurang harga barang.
 klu pakai uang jdinya nge return uang yg terakhir biar lebih make sense aja
 */
